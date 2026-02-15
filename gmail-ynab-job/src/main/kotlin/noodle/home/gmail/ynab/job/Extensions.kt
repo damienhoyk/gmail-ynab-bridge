f92699aa2806f7101444e1f7b831fd8de269a19c
Damien <damien.hoyk@gmail.com>
2026-02-15 09:16:01 +0800

    add extension

--- gmail-ynab-job/src/main/kotlin/noodle/home/gmail/ynab/job/Extensions.kt
+++ gmail-ynab-job/src/main/kotlin/noodle/home/gmail/ynab/job/Extensions.kt
@@ -5,12 +5,22 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import noodle.ynab.Transaction
 import java.io.File
 import java.net.URL
 import kotlin.io.path.toPath
 
 fun jacksonObjectMapper(jf: JsonFactory) = ObjectMapper(jf).registerKotlinModule()
 fun getResource(name: String) = {}.javaClass.getResource(name)
+
+fun List<TransactionMatcher>.parse(message: String) = firstNotNullOfOrNull { matcher -> matcher.parse(message) }
+fun List<TransactionMatcher>.parse(messages: List<String>): Pair<List<Transaction>, List<String>> {
+    val (parsed, unparsed) = messages
+        .associateWith { parse(it) }
+        .entries.partition { it.value != null }
+
+    return parsed.map { it.value!! } to unparsed.map { it.key }
+}
 
 inline fun <reified T> File.readYaml() = readText().let { jacksonObjectMapper(YAMLFactory()).readValue<T>(it) }
 inline fun <reified T> URL.readYaml() = toURI().toPath().toFile().readYaml<T>()
