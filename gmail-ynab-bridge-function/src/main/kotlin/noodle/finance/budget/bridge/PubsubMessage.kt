e10bc6704ea764c326ab7c44e8402a7eb34cfee1
Damien <damien.hoyk@gmail.com>
2026-02-25 09:25:48 +0800

    chore: remove gmail-ynab-bridge-function module and its associated files.

--- gmail-ynab-bridge-function/src/main/kotlin/noodle/finance/budget/bridge/PubsubMessage.kt
+++
@@ -1,6 +0,0 @@
-package noodle.finance.budget.bridge
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class PubsubMessage(val data: String, val messageId: String, val publishTime: String)
--- gmail-ynab-bridge-function/src/main/kotlin/noodle/finance/budget/bridge/PubsubNotification.kt
+++
@@ -1,6 +0,0 @@
-package noodle.finance.budget.bridge
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class PubsubNotification(val message: PubsubMessage, val subscription: String)
